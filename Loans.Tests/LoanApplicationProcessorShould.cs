62f66c6f4e4eedfb5df5f172c967d33874ff0deb
Alican Demirtas <demirtaa@legal.regn.net>
2021-06-17 20:21:12 +0100

    Getting Started: Creating a Second Test (that Fails)

--- Loans.Tests/LoanApplicationProcessorShould.cs
+++ Loans.Tests/LoanApplicationProcessorShould.cs
@@ -29,5 +29,29 @@ namespace Loans.Tests
 
             Assert.That(application.GetIsAccepted(), Is.False);
         }
+
+        [Test]
+        public void Accept()
+        {
+            var product = new LoanProduct(99, "Loan", 5.25m);
+            var amount = new LoanAmount("USD", 200_000);
+            var application = new LoanApplication(42,
+                                                  product,
+                                                  amount,
+                                                  "Sarah",
+                                                  25,
+                                                  "133 Pluralsight Drive, Draper, Utah",
+                                                  65_000);
+
+            var mockIdentityVerifier = new Mock<IIdentityVerifier>();
+            var mockCreditScorer = new Mock<ICreditScorer>();
+
+            var sut = new LoanApplicationProcessor(mockIdentityVerifier.Object,
+                                                   mockCreditScorer.Object);
+
+            sut.Process(application);
+
+            Assert.That(application.GetIsAccepted(), Is.True);
+        }
     }
 }
